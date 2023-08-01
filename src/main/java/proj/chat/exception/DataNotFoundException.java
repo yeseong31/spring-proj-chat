5830fa67309e70479ce01551756b59c76a0d5df2
yeseong31 <yeseong31@naver.com>
2023-08-01 22:47:48 +0900

    feat: 데이터 조회 실패 시 예외 추가

---
+++ src/main/java/proj/chat/exception/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package proj.chat.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
