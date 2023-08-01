742b48a99f27661934d6a055f270f2c4f72ffa18
yeseong31 <yeseong31@naver.com>
2023-08-01 22:48:27 +0900

    feat: DTO 추가 - MemberResponseDto

---
+++ src/main/java/proj/chat/dto/MemberResponseDto.java
@@ -0,0 +1,20 @@
+package proj.chat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import proj.chat.entity.Member;
+
+@Getter
+@NoArgsConstructor
+public class MemberResponseDto {
+    
+    private Long id;
+    private String name;
+    private String email;
+    
+    public MemberResponseDto(Member entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.email = entity.getEmail();
+    }
+}
