8e68d66e080a20568da49f27101368f91d2ea080
yeseong31 <yeseong31@naver.com>
2023-08-01 23:54:45 +0900

    feat: DB 정보를 처리하는 CustomUserDetailsService 클래스 추가

---
+++ src/main/java/proj/chat/config/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package proj.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import proj.chat.dto.MemberResponseDto;
+import proj.chat.service.MemberService;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    private final MemberService memberService;
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        MemberResponseDto dto = memberService.findByEmail(email);
+        
+        return User.builder()
+                .username(dto.getEmail())
+                .password(dto.getPassword())
+                .build();
+    }
+}
