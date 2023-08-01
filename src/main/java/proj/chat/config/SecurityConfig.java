af50fddcfebc98a476fde99bbefc175b6f7debdc
yeseong31 <yeseong31@naver.com>
2023-08-01 23:53:04 +0900

    feat: Spring Security Configuration 추가

---
+++ src/main/java/proj/chat/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package proj.chat.config;
+
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/", "/home", "/image/**", "/css/**", "/js/**",
+                                "/auth/v1/login", "/auth/v1/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/auth/v1/login")
+                        .loginProcessingUrl("/auth/v1/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/auth/v1/signup", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/auth/v1/logout")
+                        .logoutSuccessUrl("/")
+                        .logoutSuccessHandler((request, response, authentication)
+                                -> response.setStatus(HttpServletResponse.SC_OK))
+                        .invalidateHttpSession(true).deleteCookies("JSESSIONID")
+                );
+        
+        return http.build();
+    }
+    
+    /**
+     * Spring Security Authentication
+     */
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    /**
+     * Password Encoder
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
