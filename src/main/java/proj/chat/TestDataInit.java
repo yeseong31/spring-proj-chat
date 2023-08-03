93b176c56a76dbc33985049d6c1492de23fc25b1
yeseong31 <yeseong31@naver.com>
2023-08-03 16:46:25 +0900

    feat: 테스트용 사용자 데이터 추가

---
+++ src/main/java/proj/chat/TestDataInit.java
@@ -0,0 +1,36 @@
+package proj.chat;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import proj.chat.dto.MemberSaveRequestDto;
+import proj.chat.service.MemberService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+    
+    private final MemberService memberService;
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        log.info("초기화 메서드 호출");
+    
+        for (int i = 0; i < 20; i++) {
+            MemberSaveRequestDto dto = MemberSaveRequestDto.builder()
+                    .email("test" + i + "@test.com")
+                    .name("name" + i)
+                    .password("!Test" + i)
+                    .matchingPassword("!Test" + i)
+                    .build();
+    
+            Long savedId = memberService.save(dto);
+            log.info("회원{} 저장", savedId);
+        }
+        
+        log.info("초기화 메서드 종료");
+    }
+}
