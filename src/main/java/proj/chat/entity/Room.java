0043ec03c540f8dd2f90ed6e3241a487a98ac281
yeseong31 <yeseong31@naver.com>
2023-08-03 17:42:13 +0900

    feat: 사용자 참가/퇴장 시 인원 수 갱신

--- src/main/java/proj/chat/entity/Room.java
+++ src/main/java/proj/chat/entity/Room.java
@@ -3,6 +3,7 @@ package proj.chat.entity;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,16 +25,24 @@ public class Room extends BaseEntity {
     @Column(nullable = false)
     private String name;
     
+    @Builder.Default()
+    private int maxCount = 10;
     private int count;
     
     @Builder
-    public Room(String name) {
+    public Room(String name, @Nullable int maxCount) {
         this.name = name;
+        this.maxCount = maxCount;
         this.count = 0;
     }
     
     // === 비즈니스 로직 ===
     public void addCount(int n) {
+        if (n > 0 && count + n > maxCount) {
+            throw new IllegalStateException("이미 정원이 가득 찼습니다");
+        } else if (n < 0 && count + n < 0) {
+            throw new IllegalStateException("아직 방에 들어온 사람이 없습니다");
+        }
         this.count += n;
     }
 }
