b21eabf3829844d55382597393c54bfc572f58d0
yeseong31 <yeseong31@naver.com>
2023-08-03 15:43:28 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/proj/chat/controller/MemberController.java
@@ -0,0 +1,57 @@
+package proj.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import proj.chat.dto.MemberSaveRequestDto;
+import proj.chat.service.MemberService;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/auth/v1")
+public class MemberController {
+    
+    private final MemberService memberService;
+    
+    /**
+     * 회원가입(GET)
+     */
+    @GetMapping("/signup")
+    public String signupForm(@ModelAttribute MemberSaveRequestDto memberSaveRequestDto) {
+        return "signup";
+    }
+    
+    /**
+     * 회원가입(POST)
+     */
+    @PostMapping("/signup")
+    public String signup(
+            @Validated @ModelAttribute MemberSaveRequestDto dto, BindingResult bindingResult,
+            @RequestParam(defaultValue = "/") String redirectUrl) {
+    
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+        
+        // 비밀번호가 일치하지 않는 경우
+        if (!dto.getPassword().equals(dto.getMatchingPassword())) {
+            bindingResult.rejectValue("matchingPassword", "passwordIncorrect",
+                    "비밀번호가 일치하지 않습니다");
+            return "signup";
+        }
+    
+        // 회원가입 진행
+        Long savedId = memberService.save(dto);
+        log.info("회원가입 왼료: ID={}", savedId);
+    
+        return "redirect:" + redirectUrl;
+    }
+}
