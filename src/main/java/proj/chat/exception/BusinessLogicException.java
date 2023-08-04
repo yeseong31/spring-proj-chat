c68def78096c444016974abcf05ffb92a21304cb
yeseong31 <yeseong31@naver.com>
2023-08-04 13:39:29 +0900

    feat: 예외 추가 - BusinessLogicException

---
+++ src/main/java/proj/chat/exception/BusinessLogicException.java
@@ -0,0 +1,8 @@
+package proj.chat.exception;
+
+public class BusinessLogicException extends RuntimeException {
+    
+    public BusinessLogicException(String message) {
+        super(message);
+    }
+}
