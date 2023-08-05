c3bc895ba51a7daa753ec4019db0e4398f427d95
yeseong31 <yeseong31@naver.com>
2023-08-05 15:15:46 +0900

    test: 회원가입 화면 출력 테스트

---
+++ src/test/java/proj/chat/controller/AuthControllerTest.java
@@ -0,0 +1,35 @@
+package proj.chat.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+    
+    @Autowired
+    private MockMvc mvc;
+    
+    @Test
+    @DisplayName("회원가입 화면 출력")
+    void signupForm() throws Exception {
+        mvc
+                .perform(
+                        get("/auth/signup")
+//                                .param("name", "myName")
+//                                .param("value", "myValue")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("signup"));
+    }
+}
