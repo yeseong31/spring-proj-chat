f1d14ea0746fd6579862290f72558cb3c36da650
yeseong31 <yeseong31@naver.com>
2023-08-08 00:05:12 +0900

    feat: 채팅방 생성 및 목록 확인
    - 채팅방 생성 부분은 GET으로 임시 구현함
    - 추후 @PostMapping으로 동작하도록 수정할 예정

--- src/main/java/proj/chat/controller/ChatController.java
+++ src/main/java/proj/chat/controller/ChatController.java
@@ -1,17 +1,31 @@
 package proj.chat.controller;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import proj.chat.entity.ChatRoom;
+import proj.chat.service.ChatService;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/chat")
+@RequiredArgsConstructor
 public class ChatController {
     
+    private final ChatService chatService;
+    
     @GetMapping
-    public String chat() {
-        return "chat";
+    public List<ChatRoom> chat() {
+        return chatService.findAllRooms();
+    }
+    
+    // TODO: PostMapping으로 변경할 것
+    @GetMapping("/create")
+    public ChatRoom createChatRoom(@RequestParam String name) {
+        return chatService.createChatRoom(name);
     }
 }
