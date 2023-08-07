ab6c35a40591e280e93a2b67401064784da78ded
yeseong31 <yeseong31@naver.com>
2023-08-07 15:02:57 +0900

    chore: 로그 추적 - 스프링 빈 등록

--- src/main/java/proj/chat/ChatApplication.java
+++ src/main/java/proj/chat/ChatApplication.java
@@ -2,14 +2,24 @@ package proj.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import proj.chat.config.AopConfig;
+import proj.chat.aop.trace.logtrace.LogTrace;
+import proj.chat.aop.trace.logtrace.ThreadLocalLogTrace;
 
+@Import(AopConfig.class)
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "proj.chat")
 public class ChatApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+    
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
 }
