d61f6a8148e66489d610220e588b45fd62b1c455
yeseong31 <yeseong31@naver.com>
2023-08-07 12:04:41 +0900

    feat: DTO 추가 - MemberUpdateRequestDto

---
+++ src/main/java/proj/chat/dto/MemberUpdateRequestDto.java
@@ -0,0 +1,47 @@
+package proj.chat.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import proj.chat.entity.Member;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class MemberUpdateRequestDto {
+    
+    private Long id;
+    
+    private String name;
+    
+    private String email;
+    
+    private String password;
+    
+    private boolean status;
+    
+    
+    public Member dtoToEntity() {
+        return Member.builder()
+                .id(id)
+                .name(name)
+                .email(email)
+                .password(password)
+                .status(status)
+                .build();
+    }
+    
+    @Builder
+    public MemberUpdateRequestDto(Long id, String name, String email, String password,
+            String matchingPassword, boolean status) {
+        
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+    }
+}
