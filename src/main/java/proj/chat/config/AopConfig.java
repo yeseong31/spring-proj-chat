e550c95a0fc4861eb147121589249bcb687d2b82
yeseong31 <yeseong31@naver.com>
2023-08-07 15:00:22 +0900

    chore: AOP - 로그 추적 설정

---
+++ src/main/java/proj/chat/config/AopConfig.java
@@ -0,0 +1,15 @@
+package proj.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import proj.chat.aop.aspect.LogTraceAspect;
+import proj.chat.aop.trace.logtrace.LogTrace;
+
+@Configuration
+public class AopConfig {
+    
+    @Bean
+    public LogTraceAspect logTraceAspect(LogTrace logTrace) {
+        return new LogTraceAspect(logTrace);
+    }
+}
