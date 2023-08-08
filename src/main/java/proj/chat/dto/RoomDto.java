9a2c5bc680cade19928171a1eacf4d6192084faa
yeseong31 <yeseong31@naver.com>
2023-08-08 12:53:10 +0900

    rename: RoomDto로 이름 변경

--- src/main/java/proj/chat/dto/RoomDto.java
+++ src/main/java/proj/chat/dto/RoomDto.java
@@ -1,4 +1,4 @@
-package proj.chat.entity;
+package proj.chat.dto;
 
 import static lombok.AccessLevel.PROTECTED;
 import static proj.chat.dto.ChatMessageDto.MessageType.ENTER;
@@ -7,32 +7,32 @@ import static proj.chat.dto.ChatMessageDto.MessageType.TALK;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.socket.WebSocketSession;
-import proj.chat.dto.ChatMessageDto;
-import proj.chat.service.ChatService;
+import proj.chat.service.RoomService;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class ChatRoom {
+public class RoomDto {
     
-    private String roomId;
-    private String name;
+    private String roomId;    // 채팅방 ID
+    private String roomName;  // 채팅방 이름
     
     private final Set<WebSocketSession> sessions = new HashSet<>();
     
     @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
+    public RoomDto(String roomName) {
+        this.roomId = UUID.randomUUID().toString().substring(0, 8);
+        this.roomName = roomName;
     }
     
     // JSON 데이터를 받아 WebSocketChatHandler에서 해당 데이터에 담긴 roomId를 chatService에서 조회
     // 해당 roomId의 방을 찾아서 JSON 데이터에 담긴 메시지를 해당 채팅방으로 전송
     public void handlerActions(
-            WebSocketSession session, ChatMessageDto message, ChatService chatService) {
+            WebSocketSession session, ChatMessageDto message, RoomService chatService) {
         
         if (message.getType().equals(ENTER)) {
             // '입장' 이벤트인 경우
@@ -51,7 +51,7 @@ public class ChatRoom {
         }
     }
     
-    private <T> void sendMessage(T message, ChatService chatService) {
+    private <T> void sendMessage(T message, RoomService chatService) {
         sessions.parallelStream()
                 .forEach(sessions -> chatService.sendMessage(sessions, message));
     }
