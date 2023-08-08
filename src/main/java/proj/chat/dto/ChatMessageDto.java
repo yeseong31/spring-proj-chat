5f6afa85e863e1fab2ddc34079858c7f11e95885
yeseong31 <yeseong31@naver.com>
2023-08-08 12:53:59 +0900

    feat: ChatMessageDto에 빌더 추가

--- src/main/java/proj/chat/dto/ChatMessageDto.java
+++ src/main/java/proj/chat/dto/ChatMessageDto.java
@@ -1,5 +1,7 @@
 package proj.chat.dto;
 
+import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,4 +19,16 @@ public class ChatMessageDto {
     private String roomId;       // 방 ID
     private String sender;       // 채팅 송신자
     private String message;      // 메시지
+    private LocalDateTime time;  // 채팅 발송 시간
+    
+    @Builder
+    public ChatMessageDto(
+            MessageType type, String roomId, String sender, String message, LocalDateTime time) {
+        
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+        this.time = time;
+    }
 }
