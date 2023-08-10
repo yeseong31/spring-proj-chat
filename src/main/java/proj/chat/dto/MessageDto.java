14b7e1b56e7fabbf3971f009ba956295bec0b552
yeseong31 <yeseong31@naver.com>
2023-08-10 16:04:12 +0900

    chore: 수신자 대신 채널 정보를 담도록 수정

--- src/main/java/proj/chat/dto/MessageDto.java
+++ src/main/java/proj/chat/dto/MessageDto.java
@@ -7,39 +7,45 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import proj.chat.entity.MessageType;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor(access = PROTECTED)
 public class MessageDto {
     
+    public enum MessageType {
+        ENTER, TALK, LEAVE
+    }
+    
     private MessageType type;    // 메시지 타입
-    private String sender;       // 보내는 사람
-    private String receiver;     // 받는 사람
+    private String memberId;     // 사용자 ID
+    private String channelId;    // 채널 ID
     private String message;      // 메시지
     private LocalDateTime time;  // 채팅 발송 시간
     
     @Builder
-    public MessageDto(
-            MessageType type, String sender, String receiver, String message, LocalDateTime time) {
+    public MessageDto(MessageType type, String memberId, String channelId, String message,
+            LocalDateTime time) {
         
         this.type = type;
-        this.sender = sender;
-        this.receiver = receiver;
+        this.memberId = memberId;
+        this.channelId = channelId;
         this.message = message;
         this.time = time;
     }
 }
 
 /*
+
 [데이터 전송 형식(JSON)]
 
 {
-    "type": "TALK",
-    "receiver": "ff076bb7-7e80-9bbb-49e7-24d507692bc7",
-    "sender": "9bcc2479-a5ce-eb16-3c14-61c079fd170d",
-    "message": "hello22"
+"type": "ENTER",
+"memberId": "test1234",
+"channelId": "a4c2eb10-0a4d-4880-adb4-24704d0d61f2",
+"message": "hi"
 }
 
-*/
+ */
