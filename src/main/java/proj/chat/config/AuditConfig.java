ac432fa2335fd01c7625646790155e8f28e99e8f
yeseong31 <yeseong31@naver.com>
2023-08-11 15:46:55 +0900

    feat: @CreatedBy, @LastModifiedBy 컬럼 데이터를 UUID 값으로 저장

---
+++ src/main/java/proj/chat/config/AuditConfig.java
@@ -0,0 +1,17 @@
+package proj.chat.config;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+@Configuration
+public class AuditConfig implements AuditorAware<String> {
+    
+    // TODO: createdBy, lastModifiedBy 값을 UUID가 아닌 사용자 값으로 바꾸어야 함
+    
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(UUID.randomUUID().toString());
+    }
+}
