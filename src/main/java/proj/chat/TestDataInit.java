66f23f9345791c30f87b1ed61c38e972d3ab464f
yeseong31 <yeseong31@naver.com>
2023-08-11 12:09:25 +0900

    feat: 테스트용 채널 데이터 추가

--- src/main/java/proj/chat/TestDataInit.java
+++ src/main/java/proj/chat/TestDataInit.java
@@ -6,7 +6,9 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import proj.chat.dto.chat.ChannelSaveRequestDto;
 import proj.chat.dto.member.MemberSaveRequestDto;
+import proj.chat.service.ChannelService;
 import proj.chat.service.MemberService;
 
 @Slf4j
@@ -16,6 +18,7 @@ import proj.chat.service.MemberService;
 public class TestDataInit {
     
     private final MemberService memberService;
+    private final ChannelService channelService;
     
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
@@ -31,6 +34,17 @@ public class TestDataInit {
     
             Long savedId = memberService.save(dto);
             log.info("회원{} 저장", savedId);
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            ChannelSaveRequestDto dto = ChannelSaveRequestDto.builder()
+                    .name("channel" + i)
+                    .password("!Test" + i)
+                    .maxCount(5)
+                    .build();
+    
+            Long savedId = channelService.save(dto);
+            log.info("채널{} 저장", savedId);
         }
         
         log.info("초기화 메서드 종료");
