08042634a3ac02562af838b96bb9d4c801f32012
yeseong31 <yeseong31@naver.com>
2023-08-11 15:45:46 +0900

    chore: 메시지 송수신 시 채널 UUID 전달

--- src/main/java/proj/chat/dto/chat/MessageDto.java
+++ src/main/java/proj/chat/dto/chat/MessageDto.java
@@ -2,14 +2,12 @@ package proj.chat.dto.chat;
 
 import static lombok.AccessLevel.PROTECTED;
 
-import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import proj.chat.entity.Message;
-import proj.chat.entity.MessageType;
 
 @Getter
 @Setter
@@ -17,24 +15,19 @@ import proj.chat.entity.MessageType;
 @NoArgsConstructor(access = PROTECTED)
 public class MessageDto {
     
-    private MessageType type;    // 메시지 타입
-    private String memberId;     // 사용자 ID
-    private String channelId;    // 채널 ID
+    private String channelUuid;  // 채널 UUID
+    private String memberName;   // 메시지 송신자 이름
     private String content;      // 메시지
-    private LocalDateTime time;  // 채팅 발송 시간
     
     @Builder
     public MessageDto(Message entity) {
-        this.type = entity.getType();
-        this.memberId = entity.getMember().getName();
-        this.channelId = entity.getChannel().getUuid();
+        this.channelUuid = entity.getChannel().getUuid();
+        this.memberName = entity.getMember().getName();
         this.content = entity.getContent();
-        this.time = entity.getCreatedDate();
     }
     
     public Message dtoToEntity() {
         return Message.builder()
-                .type(type)
                 .content(content)
                 .build();
     }
@@ -45,10 +38,9 @@ public class MessageDto {
 [데이터 전송 형식(JSON)]
 
 {
-"type": "ENTER",
-"memberId": "test1234",
+"memberName": "test1234",
 "channelId": "a4c2eb10-0a4d-4880-adb4-24704d0d61f2",
-"message": "hi"
+"content": "hi"
 }
 
  */
