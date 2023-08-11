a1d89c3c31fb509228b2e7b61f12a6c7d1fc7c4d
yeseong31 <yeseong31@naver.com>
2023-08-11 12:13:42 +0900

    chore: 사용하지 않는 컬럼 삭제

--- src/main/java/proj/chat/entity/Message.java
+++ src/main/java/proj/chat/entity/Message.java
@@ -1,17 +1,13 @@
 package proj.chat.entity;
 
-import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,21 +18,16 @@ import lombok.ToString;
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @ToString(exclude = {"member", "channel"})
-public class Message extends BaseEntity {
+public class Message extends BaseTimeEntity {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Enumerated(STRING)
-    private MessageType type;
-    
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
     
-    @Lob
-    @Column(columnDefinition = "BLOB")
     private String content;
     
     @ManyToOne(fetch = LAZY)
@@ -44,9 +35,8 @@ public class Message extends BaseEntity {
     public Channel channel;
     
     @Builder
-    public Message(Long id, MessageType type, Member member, String content, Channel channel) {
+    public Message(Long id, Member member, String content, Channel channel) {
         this.id = id;
-        this.type = type;
         this.member = member;
         this.content = content;
         this.channel = channel;
