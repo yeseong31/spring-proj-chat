71bc427df350e19b2fd0ac3747b6b7368ecc87b4
yeseong31 <yeseong31@naver.com>
2023-08-13 13:25:01 +0900

    delete: 이메일 검증 로직 삭제
    - 이메일 인증 시 DTO로 이메일 정보가 곧바로 넘어오지 않음

--- src/main/java/proj/chat/validator/EmailVerificationRequestDtoValidator.java
+++ src/main/java/proj/chat/validator/EmailVerificationRequestDtoValidator.java
@@ -1,24 +1,14 @@
 package proj.chat.validator;
 
-import java.util.Objects;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import proj.chat.dto.email.EmailVerificationRequestDto;
-import proj.chat.entity.EmailToken;
-import proj.chat.repository.EmailTokenRepository;
-import proj.chat.repository.MemberRepository;
 
 @Component
 @RequiredArgsConstructor
 public class EmailVerificationRequestDtoValidator implements Validator {
-    
-    private final MemberRepository memberRepository;
-    private final EmailTokenRepository emailTokenRepository;
-    private final PasswordEncoder passwordEncoder;
     
     @Override
     public boolean supports(Class<?> clazz) {
@@ -29,32 +19,7 @@ public class EmailVerificationRequestDtoValidator implements Validator {
     public void validate(Object target, Errors errors) {
         EmailVerificationRequestDto dto = (EmailVerificationRequestDto) target;
         
-        // 회원가입을 진행하지 않은 사용자인 경우
-        if (!memberRepository.existsByEmail(dto.getEmail())) {
-            errors.rejectValue("email", "invalid.email",
-                    new Object[]{dto.getEmail()}, "회원가입을 먼저 진행해주세요");
-        }
+        // TODO: 토큰 유효 기간이 지난 경우
         
-        // 인증 번호가 만들어지지 않은 사용자인 경우
-        Optional<EmailToken> findEmailToken = emailTokenRepository.findByMemberEmail(dto.getEmail());
-        EmailToken emailToken = null;
-        
-        if (findEmailToken.isEmpty()) {
-            errors.rejectValue("email", "invalid.email",
-                    new Object[]{dto.getEmail()}, "회원가입을 먼저 진행해주세요");
-        } else {
-            emailToken = findEmailToken.get();
-        }
-    
-        // 토큰 값이 유효하지 않은 경우
-        if (!Objects.requireNonNull(emailToken)
-                .checkVerificationToken(dto.getToken(), passwordEncoder)) {
-            
-            errors.rejectValue("email", "invalid.token",
-                    new Object[]{dto.getEmail()}, "유효하지 않은 토큰입니다");
-        }
-    
-        // 토큰 유효 기간이 지난 경우
-        // ...
     }
 }
