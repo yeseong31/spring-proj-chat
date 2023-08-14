7e8121d049dbfc66323ba3ef865df94371f58ff3
yeseong31 <yeseong31@naver.com>
2023-08-14 09:42:36 +0900

    refactor: createdBy, lastModifiedBy 값을 사용자 이메일로 저장

--- src/main/java/proj/chat/domain/common/config/AuditConfig.java
+++ src/main/java/proj/chat/domain/common/config/AuditConfig.java
@@ -1,17 +1,26 @@
 package proj.chat.domain.common.config;
 
 import java.util.Optional;
-import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+@Slf4j
 @Configuration
 public class AuditConfig implements AuditorAware<String> {
     
-    // TODO: createdBy, lastModifiedBy 값을 UUID가 아닌 사용자 값으로 바꾸어야 함
-    
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of(UUID.randomUUID().toString());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            log.info("[getCurrentAuditor] 사용자 정보를 찾을 수 없습니다");
+            return Optional.empty();
+        }
+        
+        String authenticationName = authentication.getName();
+        log.info("[getCurrentAuditor] 사용자 정보={}", authenticationName);
+        return Optional.of(authenticationName);
     }
 }
