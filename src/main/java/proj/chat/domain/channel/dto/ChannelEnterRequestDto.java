292e83beb346004cc56c68028e00b959f4bca3e3
yeseong31 <yeseong31@naver.com>
2023-08-21 23:01:37 +0900

    feat: DTO 추가 - ChannelEnterRequestDto

---
+++ src/main/java/proj/chat/domain/channel/dto/ChannelEnterRequestDto.java
@@ -0,0 +1,21 @@
+package proj.chat.domain.channel.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ChannelEnterRequestDto {
+    
+    private String channelUuid;
+    
+    @Length(min = 4, max = 16, message = "비밀번호는 4~16자여야 합니다")
+    @NotEmpty(message = "비밀번호를 입력해 주세요")
+    private String password;
+}
