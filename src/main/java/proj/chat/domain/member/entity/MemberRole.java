3125c8b139830d333848c4a3569748209b270c6c
yeseong31 <yeseong31@naver.com>
2023-08-21 10:48:03 +0900

    feat: 사용자 이름이 "admin"인 경우 관리자 권한 부여

--- src/main/java/proj/chat/domain/member/entity/MemberRole.java
+++ src/main/java/proj/chat/domain/member/entity/MemberRole.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MemberRole {
-    MEMBER("MEMBER"),
-    ADMIN("MEMBER,ADMIN");
+    MEMBER("ROLE_MEMBER"),
+    ADMIN("ROLE_MEMBER, ROLE_ADMIN");
     
     private final String value;
 }
--- src/main/java/proj/chat/security/service/CustomUserDetailsService.java
+++ src/main/java/proj/chat/security/service/CustomUserDetailsService.java
@@ -1,12 +1,18 @@
 package proj.chat.security.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import proj.chat.domain.member.dto.MemberResponseDto;
+import proj.chat.domain.member.entity.MemberRole;
 import proj.chat.domain.member.service.MemberService;
 
 @Component
@@ -18,10 +24,20 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         MemberResponseDto dto = memberService.findByEmail(email);
+    
+        Objects.requireNonNull(dto);
+    
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        if (dto.getName().equals("admin")) {
+            authorityList.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+        } else {
+            authorityList.add(new SimpleGrantedAuthority(MemberRole.MEMBER.getValue()));
+        }
         
         return User.builder()
                 .username(dto.getEmail())
                 .password(dto.getPassword())
+                .authorities(authorityList)
                 .build();
     }
 }
