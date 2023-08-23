7bebdd95a1c753ac3098618a3491d3f7b05aac60
yeseong31 <yeseong31@naver.com>
2023-08-23 11:25:39 +0900

    fix: validator 스프링 빈 등록 누락 수정

--- src/main/java/proj/chat/domain/channel/validator/ChannelEnterRequestDtoValidator.java
+++ src/main/java/proj/chat/domain/channel/validator/ChannelEnterRequestDtoValidator.java
@@ -1,9 +1,13 @@
 package proj.chat.domain.channel.validator;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import proj.chat.domain.channel.dto.ChannelEnterRequestDto;
 
+@Component
+@RequiredArgsConstructor
 public class ChannelEnterRequestDtoValidator implements Validator {
     
     @Override
