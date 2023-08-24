b39cec0997d41ebe67cef3eb9a7eed965d687d32
yeseong31 <yeseong31@naver.com>
2023-08-25 00:19:49 +0900

    feat: 컨트롤러 추가(임시) - ChatRoomController

---
+++ src/main/java/proj/chat/websocket/controller/ChatRoomController.java
@@ -0,0 +1,25 @@
+package proj.chat.websocket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatRoomController {
+    
+    @GetMapping("/rooms")
+    public String getRooms() {
+        return "chat/rooms";
+    }
+    
+    @GetMapping(value = "/room")
+    public String getRoom(Long chatRoomId, String nickname, Model model){
+        
+        model.addAttribute("chatRoomId", chatRoomId);
+        model.addAttribute("nickname", nickname);
+        
+        return "chat/room";
+    }
+}
