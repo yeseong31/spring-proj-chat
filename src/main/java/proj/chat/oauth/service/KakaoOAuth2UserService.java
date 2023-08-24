f4fefa8ad5907a2072aad0f3fa9a1df338233ae7
yeseong31 <yeseong31@naver.com>
2023-08-24 14:09:14 +0900

    feat: 카카오 로그인 + Spring Security (구현 중)

---
+++ src/main/java/proj/chat/oauth/service/KakaoOAuth2UserService.java
@@ -0,0 +1,98 @@
+package proj.chat.oauth.service;
+
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import proj.chat.domain.member.entity.Member;
+import proj.chat.domain.member.repository.MemberRepository;
+import proj.chat.exception.DataNotFoundException;
+import proj.chat.exception.OAuthTypeMatchNotFoundException;
+import proj.chat.oauth.context.MemberContext;
+
+/**
+ * <li>loadUser() 메서드가 실행될 시점에는 이미 Kakao Access Token이 정상적으로 발급된 상태이다.</li>
+ * <li>super.loadUser() 메서드를 통해 Kakao Access Token으로 User 정보를 조히한다.</li>
+ */
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class KakaoOAuth2UserService extends DefaultOAuth2UserService {
+    
+    private final MemberRepository memberRepository;
+    private final HttpSession httpSession;
+    
+    @Override
+    @Transactional
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+    
+        log.info("[loadUser] attributes={}", attributes);
+        
+        String oAuthId = oAuth2User.getName();
+        
+        Member member = null;
+        String oAuthType = userRequest.getClientRegistration()
+                .getRegistrationId().
+                toUpperCase();
+        
+        if (!"KAKAO".equals(oAuthType)) {
+            throw new OAuthTypeMatchNotFoundException("OAuth 타입을 찾을 수 없습니다");
+        }
+        
+        if (isNew(oAuthType, oAuthId)) {
+            switch (oAuthType) {
+                case "KAKAO" -> {
+                    Map attributesProperties = (Map) attributes.get("properties");
+                    Map attributesKakaoAcount = (Map) attributes.get("kakao_account");
+                    String nickname = (String) attributesProperties.get("nickname");
+                    
+                    String email = "%s@kakao.com".formatted(oAuthId);
+                    String username = "KAKAO_%s".formatted(oAuthId);
+                    
+                    if ((boolean) attributesKakaoAcount.get("has_email")) {
+                        email = (String) attributesKakaoAcount.get("email");
+                    }
+                    
+                    member = Member.builder()
+                            .email(email)
+                            .name(nickname)
+                            .password("")
+                            .fromSocial(true)
+                            .status(true)
+                            .build();
+                    
+                    memberRepository.save(member);
+                }
+            }
+        } else {
+            member = memberRepository.findByEmail("%s_%s".formatted(oAuthType, oAuthId))
+                    .orElseThrow(() -> new DataNotFoundException("존재하지 않는 사용자입니다"));
+        }
+        
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("member"));
+        return new MemberContext(member, authorities, attributes, userNameAttributeName);
+    }
+    
+    private boolean isNew(String oAuthType, String oAuthId) {
+        return memberRepository.findByEmail("%s_%s".formatted(oAuthType, oAuthId)).isEmpty();
+    }
+}
