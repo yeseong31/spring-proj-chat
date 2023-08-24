6234f223f8bd2adf23d9e674c4f7ab96b477d936
yeseong31 <yeseong31@naver.com>
2023-08-25 00:19:25 +0900

    feat: RabbitMQ 채팅을 위한 설정 추가

---
+++ src/main/java/proj/chat/websocket/config/RabbitConfig.java
@@ -0,0 +1,119 @@
+package proj.chat.websocket.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import proj.chat.websocket.handler.Receiver;
+
+@Configuration
+@EnableRabbit
+public class RabbitConfig {
+    
+    public static final String CHAT_QUEUE_NAME = "chat.queue";
+    public static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+    private static final String ROUTING_KEY = "room.*";
+    
+    @Value("${spring.rabbitmq.stream.host}")
+    private String RABBITMQ_HOST;
+    
+    @Value("${spring.rabbitmq.stream.username}")
+    private String RABBITMQ_USERNAME;
+    
+    @Value("${spring.rabbitmq.stream.password}")
+    private String RABBITMQ_PASSWORD;
+    
+    /**
+     * Queue 등록
+     */
+    @Bean
+    public Queue queue() {
+        return new Queue(CHAT_QUEUE_NAME, true);
+    }
+    
+    /**
+     * Exchange 등록
+     */
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(CHAT_EXCHANGE_NAME);
+    }
+    
+    /**
+     * Exchange와 Queue 바인딩
+     */
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+    
+    /**
+     * Jackson Date
+     */
+    @Bean
+    public com.fasterxml.jackson.databind.Module dateTimeModule() {
+        return new JavaTimeModule();
+    }
+    
+    /**
+     * Spring에서 자동 생성해 주는 ConnectionFactory는 SimpleConnectionFactory이다. 여기서는
+     * CachingConnectionFactory를 사용하므로 이를 등록해 준다.
+     */
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost(RABBITMQ_HOST);
+        factory.setUsername(RABBITMQ_USERNAME);
+        factory.setPassword(RABBITMQ_PASSWORD);
+        return factory;
+    }
+    
+    /**
+     * messageConverter 커스터마이징
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+    
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        rabbitTemplate.setRoutingKey(CHAT_QUEUE_NAME);
+        return rabbitTemplate;
+    }
+    
+    @Bean
+    public SimpleMessageListenerContainer container(MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.setQueueNames(CHAT_QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+    
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+    
+    private Jackson2JsonMessageConverter jsonMessageConverter() {
+    
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+        objectMapper.registerModule(dateTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
--- src/main/java/proj/chat/websocket/config/StompWebSocketConfig.java
+++ src/main/java/proj/chat/websocket/config/StompWebSocketConfig.java
@@ -1,38 +1,48 @@
 package proj.chat.websocket.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-/**
- * WebSocket 메시지 브로커 설정
- */
 @Configuration
 @EnableWebSocketMessageBroker
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
-    /**
-     * 엔드포인트 등록
-     *
-     * @param registry 레지스트리
-     */
+    private static final int STOMP_CLIENT_PORT = 61613;
+    
+    @Value("${RABBITMQ_HOST}")
+    private String RABBITMQ_HOST;
+    
+    @Value("${RABBITMQ_USERNAME}")
+    private String RABBITMQ_USERNAME;
+    
+    @Value("${RABBITMQ_PASSWORD}")
+    private String RABBITMQ_PASSWORD;
+    
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws-stomp")
-                .setAllowedOrigins("*");
+                .setAllowedOriginPatterns("http://*.*.*.*:8081", "http://*.*.*.*:8082")
+                .withSockJS();
     }
     
-    /**
-     * 메시지 브로커 설정
-     *
-     * @param registry 레지스트리
-     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.setPathMatcher(new AntPathMatcher("."));
+        
         registry.setApplicationDestinationPrefixes("/pub");
+        
+        // registry.enableSimpleBroker("/sub");
+        registry.enableStompBrokerRelay(
+                "/queue", "/topic", "/exchange", "/amq/queue")
+                .setRelayPort(STOMP_CLIENT_PORT)
+                .setRelayHost(RABBITMQ_HOST)
+                .setClientPasscode(RABBITMQ_PASSWORD)
+                .setClientLogin(RABBITMQ_USERNAME);
     }
 }
