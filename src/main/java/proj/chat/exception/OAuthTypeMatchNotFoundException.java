2f11feedbc98a18406d786581a764d1dacfe72d5
yeseong31 <yeseong31@naver.com>
2023-08-24 13:46:25 +0900

    feat: 예외 추가 - OAuthTypeMatchNotFoundException

---
+++ src/main/java/proj/chat/exception/OAuthTypeMatchNotFoundException.java
@@ -0,0 +1,8 @@
+package proj.chat.exception;
+
+public class OAuthTypeMatchNotFoundException extends RuntimeException {
+    
+    public OAuthTypeMatchNotFoundException(String message) {
+        super(message);
+    }
+}
