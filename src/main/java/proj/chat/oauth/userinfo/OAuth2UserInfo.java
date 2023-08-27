8a242530dfba076c11ec127637bf19460a28fa67
yeseong31 <yeseong31@naver.com>
2023-08-27 18:11:43 +0900

    feat: OAuth 로그인 정보 확인용 클래스 추가

---
+++ src/main/java/proj/chat/oauth/userinfo/OAuth2UserInfo.java
@@ -0,0 +1,16 @@
+package proj.chat.oauth.userinfo;
+
+import java.util.Map;
+
+public interface OAuth2UserInfo {
+    
+    Map<String, Object> getAttributes();
+    
+    String getProviderId();
+    
+    String getProvider();
+    
+    String getEmail();
+    
+    String getName();
+}
