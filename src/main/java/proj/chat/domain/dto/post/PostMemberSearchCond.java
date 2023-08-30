97ab80a2c64a3ba0793e8b3f9f94e1e35ad40788
yeseong31 <yeseong31@naver.com>
2023-08-30 21:02:05 +0900

    feat: 게시글 검색 Cond 추가

---
+++ src/main/java/proj/chat/domain/dto/post/PostMemberSearchCond.java
@@ -0,0 +1,13 @@
+package proj.chat.domain.dto.post;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostMemberSearchCond {
+    
+    private String keyword;
+}
