47b0300e43585bdc80b0e2939c62cad6b17bc613
yeseong31 <yeseong31@naver.com>
2023-08-31 16:00:26 +0900

    chore: 게시글 저장/수정 DTO에 @Setter 추가

--- src/main/java/proj/chat/domain/dto/post/PostUpdateRequestDto.java
+++ src/main/java/proj/chat/domain/dto/post/PostUpdateRequestDto.java
@@ -1,14 +1,20 @@
 package proj.chat.domain.dto.post;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class PostUpdateRequestDto {
     
+    @NotBlank(message = "제목을 입력해주세요")
     private String title;
+    
+    @NotBlank(message = "내용을 입력해주세요")
     private String content;
     
     @Builder
