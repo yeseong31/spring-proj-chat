735246bc47a54d847fe737e229d7df71ccf2bf0a
yeseong31 <yeseong31@naver.com>
2023-08-31 21:14:04 +0900

    feat: 답변과 사용자를 통해 답변 추천 정보 조회

--- src/main/java/proj/chat/domain/repository/VoterCommentRepository.java
+++ src/main/java/proj/chat/domain/repository/VoterCommentRepository.java
@@ -1,8 +1,14 @@
 package proj.chat.domain.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import proj.chat.domain.entity.Comment;
+import proj.chat.domain.entity.Member;
 import proj.chat.domain.entity.VoterComment;
 
 public interface VoterCommentRepository extends JpaRepository<VoterComment, Long> {
-
+    
+    Optional<VoterComment> findByCommentAndMember(Comment comment, Member member);
+    
+    boolean existsByCommentAndMember(Comment comment, Member member);
 }
