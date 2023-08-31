ebe99eed348261ad47c112b0a528ca4d106b3b1c
yeseong31 <yeseong31@naver.com>
2023-08-31 15:24:02 +0900

    chore: 답변 DTO에 사용자 이메일, 수정일 추가

--- src/main/java/proj/chat/domain/dto/comment/CommentResponseDto.java
+++ src/main/java/proj/chat/domain/dto/comment/CommentResponseDto.java
@@ -3,9 +3,11 @@ package proj.chat.domain.dto.comment;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import proj.chat.domain.entity.Comment;
 
 @Getter
+@ToString
 @NoArgsConstructor
 public class CommentResponseDto {
     
@@ -13,15 +15,19 @@ public class CommentResponseDto {
     private String content;
     private Long memberId;
     private String memberName;
+    private String memberEmail;
     private Long postId;
     private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
     
     public CommentResponseDto(Comment entity) {
         this.id = entity.getId();
         this.content = entity.getContent();
         this.memberId = entity.getMember().getId();
         this.memberName = entity.getMember().getName();
+        this.memberEmail = entity.getMember().getEmail();
         this.postId = entity.getPost().getId();
         this.createdDate = entity.getCreatedDate();
+        this.lastModifiedDate = entity.getLastModifiedDate();
     }
 }
