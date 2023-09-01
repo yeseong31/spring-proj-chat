42f8c3aafaed9855792253f65a1cd21f1bfc8e0d
yeseong31 <yeseong31@naver.com>
2023-09-01 15:59:51 +0900

    remove: 사용하지 않는 로직 삭제(임시)

--- src/main/java/proj/chat/domain/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package proj.chat.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import proj.chat.domain.entity.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-
-}
